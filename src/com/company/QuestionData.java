76d884e94abc42673b9cb29b08ceb791f5aa9e0b
Lytecyde <mik.seljama.nagaoka@gmail.com>
2016-02-11 17:55:20 +0200

    Rejected commit: Default (3)

--- src/com/company/QuestionData.java
+++ src/com/company/QuestionData.java
@@ -4,6 +4,54 @@ package com.company;
  * Created by diku on 11/26/15.
  */
 public final class QuestionData {
+
+    public static String[] ASRIitemENG = {
+            "It’s hard for me to notice when I’ve ―had enough (sweets, food, etc.). ",
+            "When I’m sad, I can usually start doing something that will make me feel better.",
+            "If something isn’t going according to my plans, I change my actions to try and reach my goal.",
+            "I can find ways to make myself study even when my friends want to go out.",
+            "I lose track of the time when I’m doing something fun.",
+
+            "When I’m bored I fidget or can’t sit still.",
+            "It’s hard for me to get started on big projects that require planning in advance.",
+            "I can usually act normal around everybody if I’m upset with someone.",
+            "I am good at keeping track of lots of things going on around me, even when I’m feeling stressed.",
+            "When I’m having a tough day, I stop myself from whining about it to my family or friends",
+
+            "I can start a new task even if I’m already tired.",
+            "I lose control whenever I don’t get my way.",
+            "Little problems detract me from my long-term plans",
+            "I forget about whatever else I need to do when I’m doing something really fun.",
+            "If I really want something, I have to have it right away.",
+
+            "During a dull class, I have trouble forcing myself to start paying attention.",
+            "After I’m interrupted or distracted, I can easily continue working where I left off.",
+            "If there are other things going on around me, I find it hard to keep my attention focused on whatever I’m doing",
+            "I never know how much more work I have to do.",
+            "When I have a serious disagreement with someone, I can talk calmly about it without losing control.",
+
+            "It’s hard to start making plans to deal with a big project or problem, especially when I’m feeling stressed.",
+            "I can calm myself down when I’m excited or all wound up.",
+            "I can stay focused on my work even when it’s dull.",
+            "I usually know when I’m going to start crying.",
+            "I can stop myself from doing things like throwing objects when I’m mad.",
+
+            "I work carefully when I know something will be tricky.",
+            "I am usually aware of my feelings before I let them out.",
+            "In class, I can concentrate on my work even if my friends are talking.",
+            "When I’m excited about reaching a goal (e.g., getting my driver’s license, going to college), it’s easy " +
+                    "to start working toward it.",
+            "I can find a way to stick with my plans and goals, even when it’s tough.",
+
+            "When I have a big project, I can keep working on it.",
+            "I can usually tell when I’m getting tired or frustrated.",
+            "I get carried away emotionally when I get excited about something.",
+            "I have trouble getting excited about something that’s really special when I’m tired.",
+            "It’s hard for me to keep focused on something I find unpleasant or upsetting.",
+
+            "I can resist doing something when I know I shouldn’t do it."
+    };
+
     public static String[] ASRIitem = {
             "Mul on raske märgata kui mul küllalt saab (magusat, toitu, seksi)",
             "Kui ma olen kurb, siis võtan midagi ette, et tuju paremaks teha.",
@@ -58,7 +106,88 @@ public final class QuestionData {
             false,false,false,false,false,
             false,false,false,true,true,
             false};
+    public static String[] SRQitemENG = {
+            "1. I usually keep track of my progress toward my goals.",
+            "2. My behavior is not that different from other people's.",
+            "3. Others tell me that I keep on with things too long.",
+            "4. I doubt I could change even if I wanted to.",
+            "5. I have trouble making up my mind about things.",
+            "6. I get easily distracted from my plans." ,
+            "7. I reward myself for progress toward my goals." ,
+            "8. I don't notice the effects of my actions until it's too late.",
+            "9. My behavior is similar to that of my friends." ,
+            "10. It's hard for me to see anything helpful about changing my ways.",
 
+            "11. I am able to accomplish goals I set for myself. Searching",
+            "12. I put off making decisions."  ,
+            "13. I have so many plans that it's hard for me to focus on any one of them.",
+
+            "14. I change the way I do things when I see a problem with how things are going.",
+
+            "15. It's hard for me to notice when I've had enough (alcohol, food, sweets).",
+
+            "16. I think a lot about what other people think of me." ,
+            "17. I am willing to consider other ways of doing things." ,
+            "18. If I wanted to change, I am confident that I could do it. Searching",
+            "19. When it comes to deciding about a change, I feel overwhelmed by the choices.",
+
+            "20. I have trouble following through with things once I've made up my mind to do something.",
+
+            "21. I don't seem to learn from my mistakes.",
+            "22. I'm usually careful not to overdo it when working, eating, drinking.",
+            
+            "23. I tend to compare myself with other people.",
+            "24. I enjoy a routine, and like things to stay the same.",
+            "25. I have sought out advice or information about changing.",
+            "26. I can come up with lots of ways to change, but it's hard for me to decide which one to use.",
+
+            "27. I can stick to a plan that's working well. ",
+            "28. I usually only have to make a mistake one time in order to learn from it.",
+
+            "29. I don't learn well from punishment.",
+            "30. I have personal standards, and try to live up to them.",
+            "31. I am set in my ways.",
+            "32. As soon as I see a problem or challenge, I start looking for possible solutions.",
+
+            "33. I have a hard time setting goals for myself.",
+            "34. I have a lot of willpower." ,
+            "35. When I'm trying to change something, I pay a lot of attention to how I'm doing.",
+
+            "36. I usually judge what I'm doing by the consequences of my actions.",
+
+            "37. I don't care if I'm different from most people.",
+            "38. As soon as I see things aren't going right I want to do something about it.",
+
+            "39. There is usually more than one way to accomplish something." ,
+            "40. I have trouble making plans to help me reach my goals.",
+            "41. I am able to resist temptation." ,
+
+            "42. I set goals for myself and keep track of my progress. ",
+            "43. Most of the time I don't pay attention to what I'm doing.",
+            "44. I try to be like people around me. ",
+            "45. I tend to keep doing the same thing, even when it doesn't work.",
+            "46. I can usually find several different possibilities when I want to change something.",
+
+            "47. Once I have a goal, I can usually plan how to reach it." ,
+            "48. I have rules that I stick by no matter what. ",
+            "49. If I make a resolution to change something, I pay a lot of attention to how I'm doing.",
+            "50. Often I don't notice what I'm doing until someone calls it to my attention.",
+
+            "51. I think a lot about how I'm doing. ",
+            "52. Usually I see the need to change before others do.",
+            "53. I'm good at finding different ways to get what I want. ",
+            "54. I usually think before I act. ",
+            "55. Little problems or distractions throw me off course.",
+            "56. I feel bad when I don't meet my goals. ",
+            "57. I learn from my mistakes. ",
+            "58. I know how I want to be. ",
+            "59. It bothers me when things aren't the way I want them.",
+            "60. I call in others for help when I need it. ",
+            "61. Before making a decision, I consider what is likely to happen if I do one thing or another.",
+
+            "62. I give up quickly.",
+            "63. I usually decide to change and hope for the best."
+    };
     public static String[] SRQitem = {
             "1. Tavaliselt märgin kirja oma edusammud eesmärgi poole.", //
             "2. Minu käitumine ei erine eriti teiste omast. ",          //r
